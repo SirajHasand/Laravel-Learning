a3544a1582b93cfcf6cbd40b9d6495f6628a76e2
sirajhasand <sirajuddinhasand@gmail.com>
2026-06-20 12:36:40 +0430

    Add route for saving notes in the HomeController

--- Laravel-Projects/blog/routes/web.php
+++ Laravel-Projects/blog/routes/web.php
@@ -6,5 +6,6 @@ use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/',[HomeController::class,'index']);
+Route::post('/save',[HomeController::class,'save']);
 Route::get('/about',[AboutController::class,'index']);
 Route::get('/contact',[ContactController::class,'index']);
