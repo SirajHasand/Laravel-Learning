fabbf45ceb2166c74661ac2300862a5412342d3e
sirajhasand <sirajuddinhasand@gmail.com>
2026-06-28 18:03:12 +0430

    Add route names for edit, update, and delete methods in HomeController

--- Laravel-Projects/blog/routes/web.php
+++ Laravel-Projects/blog/routes/web.php
@@ -7,8 +7,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/',[HomeController::class,'index']);
 Route::post('/save',[HomeController::class,'save']);
-Route::get('/edit/{id}',[HomeController::class,'edit']);
-Route::post('/update/{id}',[HomeController::class,'update']);
-Route::get('/delete/{id}',[HomeController::class,'delete']);
+Route::get('/edit/{id}',[HomeController::class,'edit'])->name('edit');
+Route::put('/update/{id}',[HomeController::class,'update'])->name('update');
+Route::delete('/delete/{id}',[HomeController::class,'delete'])->name('delete');
 Route::get('/about',[AboutController::class,'index']);
 Route::get('/contact',[ContactController::class,'index']);
