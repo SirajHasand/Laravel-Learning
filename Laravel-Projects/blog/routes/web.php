12023b451b87c9769bf6a45081403ec02276f600
sirajhasand <sirajuddinhasand@gmail.com>
2026-06-28 17:45:53 +0430

    Refactor routes to include edit, update, and delete functionality for notes

--- Laravel-Projects/blog/routes/web.php
+++ Laravel-Projects/blog/routes/web.php
@@ -7,5 +7,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/',[HomeController::class,'index']);
 Route::post('/save',[HomeController::class,'save']);
+Route::get('/edit/{id}',[HomeController::class,'edit']);
+Route::post('/update/{id}',[HomeController::class,'update']);
+Route::get('/delete/{id}',[HomeController::class,'delete']);
 Route::get('/about',[AboutController::class,'index']);
 Route::get('/contact',[ContactController::class,'index']);
