037be81cd34581b12f8d670cceff79287a987864
sirajhasand <sirajuddinhasand@gmail.com>
2026-07-01 20:44:50 +0430

    Refactor routes by removing unused controllers and simplifying route definitions

--- Laravel-Projects/blog/routes/web.php
+++ Laravel-Projects/blog/routes/web.php
@@ -1,15 +1,8 @@
 <?php
 
-use App\Http\Controllers\AboutController;
-use App\Http\Controllers\ContactController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/',[HomeController::class,'index']);
-Route::post('/save',[HomeController::class,'save']);
-Route::get('/edit/{id}',[HomeController::class,'edit'])->name('edit');
-Route::put('/update/{id}',[HomeController::class,'update'])->name('update');
-Route::delete('/delete/{id}',[HomeController::class,'delete'])->name('delete');
-Route::get('/contact',[ContactController::class,'index']);
-
-Route::resource('about', AboutController::class);
+Route::get('/',[HomeController::class,'index'])->name("home");
+Route::get('/post',[PostController::class,'index'])->name("post");
